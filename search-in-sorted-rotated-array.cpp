5ec4c54a8898d063a30ce87e849eb3e247cbe520
vaibhav <vaibhavkrpandey111@gmail.com>
2023-01-24 20:45:07 +0530

    new commit

---
+++ search-in-sorted-rotated-array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int n = nums.size();
+    int l = 0, h = n-1;
+    while(l <= h){
+        int mid = (l + h)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[l] <= nums[mid]){
+            if(target >= nums[l] && target <= nums[mid]){
+                h = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        else {
+            if(target >= nums[mid] && target <= nums[h]){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
+            }
+            
+        }
+    }
+        return -1;
+    }
+
+int main(){
+    return 0;
+}
