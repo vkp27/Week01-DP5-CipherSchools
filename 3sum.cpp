3f3c008f77507d85f209f69a559a4379a8bb5336
vaibhav <vaibhavkrpandey111@gmail.com>
2023-01-25 00:08:40 +0530

    new commit

---
+++ 3sum.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> triplets(vector<int> &nums){
+    if(nums.size() <= 2){
+        return {};
+    }
+
+    sort(nums.begin(), nums.end());
+
+    vector<vector<int>> result;
+
+    for(int i=0;i<nums.size()-2;i++){
+        int curr1 = nums[i];
+        int start = i + 1, end = nums.size() - 1;
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(curr1 + nums[start] + nums[end] == 0){
+                result.push_back({curr1, nums[start], nums[end]});
+                start++;
+                end--;
+
+                while(start < end && nums[start - 1] == nums[start]){
+                    start++;
+                }
+
+                while(start < end && nums[end] == nums[end + 1]){
+                    end--;
+                }
+            }
+            else if(curr1 + nums[start] + nums[end] < 0){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+    }
+}
+
+int main(){
+    return 0;
+}
